cce03939d14a121462b5664a8834e15b7e7aa85b
izael gomes da silva <izaelgomes127@gmail.com>
2023-07-07 17:59:42 -0300

    feat: user authenticate use case

---
+++ src/use-cases/authenticate.ts
@@ -0,0 +1,32 @@
+import { UsersRepository } from "@/repositories/users-repository"
+import { InvalidCredentialsError } from "./erros/invalid-credentials-error"
+import { compare } from "bcryptjs"
+
+
+interface AuthenticateUseCaseRequest{
+    email:string;
+    password:string;
+}
+
+type AuthenticateUseCaseReponse = void
+
+export class AuthenticateUseCase{
+
+    constructor( private usersRepository:UsersRepository){}
+    
+    async execute({email, password} : AuthenticateUseCaseRequest): Promise<AuthenticateUseCaseReponse> {
+        const user = await this.usersRepository.findByEmail(email)
+
+        if(!user) {
+            throw new InvalidCredentialsError()
+        }
+
+        const doesPasswordMatches = await compare(password, user.password_hash)
+
+        if(!doesPasswordMatches) {
+            throw new InvalidCredentialsError
+        }
+
+        return 
+    }
+}
