03374ce00259a7eec9be27fa0706fb80a491204a
izael gomes da silva <izaelgomes127@gmail.com>
2023-07-06 23:06:33 -0300

    chore: vitte for unity tests

--- src/repositories/prisma/prisma-users-repository.ts
+++ src/repositories/prisma/prisma-users-repository.ts
@@ -1,11 +1,9 @@
 import { prisma } from "@/lib/prisma"
 import { Prisma, User } from "@prisma/client"
 import { UsersRepository } from "../users-repository"
-import { GetResult } from "@prisma/client/runtime"
 
 export class PrismaUsersRepository implements UsersRepository {
     async findByEmail(email: string): Promise<User | null> {
-      
         const userWithSameEmail = await prisma.user.findUnique({
             where: {
                 email,
@@ -13,8 +11,8 @@ export class PrismaUsersRepository implements UsersRepository {
         })
 
         return userWithSameEmail
-
     }
+
     async create(data: Prisma.UserCreateInput): Promise<User> {
         const user = await prisma.user.create({
             data
